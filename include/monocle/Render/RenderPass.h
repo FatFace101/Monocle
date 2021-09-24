88e4e196b326c5470ca7df39a0f1521aa5674c68
unknown <isaac.l994@stu.nebo.edu>
2021-09-23 22:28:35 -0600

    Broke stuff
    That's basically it

--- include/monocle/Render/RenderPass.h
+++ include/monocle/Render/RenderPass.h
@@ -8,21 +8,13 @@
 
 namespace mncl {
 
-template<class TriggerType> 
-class RenderPass {
 
-	void render();
+ 
+class RenderPass {
+	
+	virtual void render() = 0;
 
 public:
-	TriggerType *subpassTrigger;
-	// Designates a new subpass with an associated trigger.
-	void addSubpass(TriggerType *trigger);
-
-	// Determines whether SceneObject obj should be rendered for this subpass.
-	virtual bool cullObject(SceneObject obj) = 0;
-	
-	// Loads uniforms to a ShaderProgram given a SceneObject and the current subpass trigger.
-	virtual void loadUniforms(ShaderProgram program, SceneObject obj) = 0;
 	
 
 };
---
+++ include/monocle/Render/Scene.h
@@ -0,0 +1,25 @@
+#ifndef MONOCLE_SCENE_H
+#define MONOCLE_SCENE_H
+
+#include <vector>
+
+#include "monocle/Render/SceneObject.h"
+
+
+namespace mncl {
+
+
+class Scene {
+	std::vector<SceneObject> objects;
+	
+
+
+
+
+};
+
+
+
+}
+
+#endif
